04404e862b8a95d33860c993cd462a36dd2a16d5
Dmitrii Nikoshin <d.nikoshin@gmail.com>
2025-10-29 22:04:44 +0300

    module3-task1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,35 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../mocks/mocks';
+
 import PageMain from '../../pages/page-main/page-main';
+import PageFavorites from '../../pages/page-favorites/page-favorites';
+import PageLogin from '../../pages/page-login/page-login';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
+import PageOffer from '../../pages/page-offer/page-offer';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   placesNumber: number;
+  authStatus: AuthorizationStatus;
 }
 
-function App({placesNumber}: AppProps): JSX.Element {
+function App({placesNumber, authStatus}: AppProps): JSX.Element {
   return (
-    <PageMain placesNumber={placesNumber}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<PageMain placesNumber={placesNumber} authStatus={authStatus} />} />
+        <Route path={AppRoute.Login} element={<PageLogin />} />
+        <Route path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authStatus={authStatus}>
+              <PageFavorites />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoute.Offer} element={<PageOffer authStatus={authStatus} />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
